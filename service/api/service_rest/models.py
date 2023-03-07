55e4778d8aa8e8dd64f31e081156c10eaa7932c2
Shaun <sragasa97@gmail.com>
2023-03-07 11:39:42 -0500

    Shaun: Finishing up views, issue with creating appointment due to status

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ManufacturerVO(models.Model):
@@ -42,17 +43,35 @@ class AutomobileVO(models.Model):
 
 class Technician(models.Model):
     name = models.CharField(max_length=100)
-    employee_num = models.PositiveIntegerField()
+    employee_num = models.PositiveIntegerField(unique=True)
 
     def __str__(self):
         return f"{self.name} | {self.employee_num}"
 
+    def get_api_url(self):
+        return reverse("api_detail_technicians", kwargs={"id": self.id})
+
+
+class Status(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name_plural = "statuses"
+
 
 class ServiceAppointment(models.Model):
     vin = models.CharField(max_length=17)
-    owner_name = models.CharField(max_length=100)
+    customer_name = models.CharField(max_length=100)
     reason = models.TextField()
-    date_time = models.DateTimeField()
+    date = models.DateField()
+    time = models.TimeField()
+    vip = models.BooleanField(default=False)
+    status = models.CharField(blank=True)
 
     technician = models.ForeignKey(
         Technician,
@@ -60,5 +79,39 @@ class ServiceAppointment(models.Model):
         on_delete=models.PROTECT,
     )
 
+    @classmethod
+    def create(cls, **kwargs):
+        kwargs["status"] = Status.objects.get(name="IN PROGRESS")
+        serviceappointment = cls(**kwargs)
+        serviceappointment.save()
+        return serviceappointment
+
+    status = models.ForeignKey(
+        Status,
+        related_name="appointments",
+        on_delete=models.PROTECT,
+    )
+
+    # def progress(self):
+    #     status = Status.objects.get(name="IN PROGRESS")
+    #     self.status = status
+    #     self.save()
+
+    def finish(self):
+        status = Status.objects.get(name="FINISHED")
+        self.status = status
+        self.save()
+
+    def cancel(self):
+        status = Status.objects.get(name="CANCELLED")
+        self.status = status
+        self.save()
+
+    def get_status(self):
+        return self.status.name
+
     def __str__(self):
-        return f"VIN:{self.vin}, WHEN:{self.date_time}, TECH:{self.technician}"
+        return f"Appt: {self.id}, VIN: {self.vin}, Customer: {self.customer_name}"
+
+    def get_api_url(self):
+        return reverse("api_detail_appointments", kwargs={"id": self.id})
--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from django.urls import path
 
-# from .views import
+from .views import (
+    api_technicians,
+    api_detail_technicians,
+    api_statuses,
+    api_detail_statuses,
+    api_appointments,
+    api_detail_appointments,
+)
 
 urlpatterns = [
-    # path("technicians/", api_list_technicians, name="api_list_technicians"),
-    # path("technicians/<int:id>/", api_detail_technicians, name="api_detail_technicians"),
-    # path("appointments/", api_list_appointments, name="api_list_appointments"),
-    # path("appointments/<int:id>/", api_detail_appointments, name="api_detail_appointments"),
+    path("technicians/", api_technicians, name="api_technicians"),
+    path(
+        "technicians/<int:id>/",
+        api_detail_technicians,
+        name="api_detail_technicians",
+    ),
+    path("statuses/", api_statuses, name="api_statuses"),
+    path("statuses/<str:name>/", api_detail_statuses, name="api_detail_statuses"),
+    path("appointments/", api_appointments, name="api_appointments"),
+    path(
+        "appointments/<int:id>/",
+        api_detail_appointments,
+        name="api_detail_appointments",
+    ),
 ]
