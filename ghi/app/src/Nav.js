8a13086d1c62bb8b02128994bfeca2887c9e883d
unknown <tbb257@nyu.edu>
2023-03-07 20:31:19 -0500

    Navigation Bar Set Up

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,76 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Manufacturers
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <NavLink className="dropdown-item" to="manufacturers">List of Manufacturers</NavLink>
+                <NavLink className="dropdown-item" to="manufacturers/create/">Add a Manufacturer</NavLink>
+              </div>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Vehicles
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <NavLink className="dropdown-item" to="vehicles">List of Vehicles</NavLink>
+              <NavLink className="dropdown-item" to="vehicles/create/">Add a Vehicle Models</NavLink>
+              </div>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Automobiles
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <NavLink className="dropdown-item" to="automobiles">List of Automobiles</NavLink>
+              <NavLink className="dropdown-item" to="automobiles/create/">Add an Automobile</NavLink>
+              </div>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Services
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <NavLink className="dropdown-item" to="service">List of Appointments</NavLink>
+                <NavLink className="dropdown-item" to="service/create/">Add an Appointment</NavLink>
+              </div>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sales
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <NavLink className="dropdown-item" to="sales">List of Sales</NavLink>
+                <NavLink className="dropdown-item" to="sales/create/">Create a Sale Record</NavLink>
+                <NavLink className="dropdown-item" to="salesperson">View a Sales Person's History</NavLink>
+                <NavLink className="dropdown-item" to="customers/create/">Add a Potential Customer</NavLink>
+                <NavLink className="dropdown-item" to="salesperson/create/">Add a Sales Person</NavLink>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="dropdown">
+              <button className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Add
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <NavLink className="dropdown-item" to="/">Add a Technician</NavLink>
+                <NavLink className="dropdown-item" to="salesperson/create/">Add a Sales Person</NavLink>
+                <NavLink className="dropdown-item" to="customers/create">Add a Customer</NavLink>
+              </div>
+            </div>
+          </li>
           </ul>
         </div>
       </div>
--- ghi/app/src/components/service/AppointmentList.js
+++ ghi/app/src/components/service/AppointmentList.js
@@ -7,40 +7,40 @@ function AppointmentList(props) {
             return "&#9744;"
         }
     }
-
-    return (
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-                <th scope="col">VIP</th>
-                <th scope="col">VIN</th>
-                <th scope="col">Customer Name</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-                <th scope="col">Technician</th>
-                <th scope="col">Reason</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.appointments.map(appointment => {
-              return (
-                <tr scope="row" key={appointment.href}>
-                  <td>{(appointment.vip) => vip(appointment.vip)}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-    )
+    
+    // return (
+    //     <table className="table table-striped table-hover">
+    //       <thead>
+    //         <tr>
+    //             <th scope="col">VIP</th>
+    //             <th scope="col">VIN</th>
+    //             <th scope="col">Customer Name</th>
+    //             <th scope="col">Date</th>
+    //             <th scope="col">Time</th>
+    //             <th scope="col">Technician</th>
+    //             <th scope="col">Reason</th>
+    //             <th scope="col"></th>
+    //             <th scope="col"></th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         {props.appointments.map(appointment => {
+    //           return (
+    //             <tr scope="row" key={appointment.href}>
+    //               <td>{(appointment.vip) => vip(appointment.vip)}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //               <td>{}</td>
+    //             </tr>
+    //           )
+    //         })}
+    //       </tbody>
+    //     </table>
+    // )
 }
 
 export default AppointmentList;
