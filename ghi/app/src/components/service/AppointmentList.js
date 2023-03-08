0add7471d0a115d781d065b2e28f6198bda0de32
Shaun <sragasa97@gmail.com>
2023-03-07 20:59:46 -0500

    Still working on services front end

--- ghi/app/src/components/service/AppointmentList.js
+++ ghi/app/src/components/service/AppointmentList.js
@@ -2,10 +2,24 @@ function AppointmentList(props) {
 
     const vip = (value) => {
         if (value) {
-            return "&#9745;"
+            return <td>&#9745;</td>
         } else {
-            return "&#9744;"
+            return <td>&#9744;</td>
         }
+    }
+
+    // const time = (value) => {
+    //     console.log(value)
+    //     let date = new Date(value)
+    //     return date.toDateString()
+    // }
+
+    const handleCancel = () => {
+        console.log('pass')
+    }
+
+    const handleFinish = () => {
+        console.log('pass')
     }
 
     return (
@@ -15,8 +29,8 @@ function AppointmentList(props) {
                 <th scope="col">VIP</th>
                 <th scope="col">VIN</th>
                 <th scope="col">Customer Name</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
+                {/* <th scope="col">Date</th>
+                <th scope="col">Time</th> */}
                 <th scope="col">Technician</th>
                 <th scope="col">Reason</th>
                 <th scope="col"></th>
@@ -27,14 +41,15 @@ function AppointmentList(props) {
             {props.appointments.map(appointment => {
               return (
                 <tr scope="row" key={appointment.href}>
-                  <td>{(appointment.vip) => vip(appointment.vip)}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
-                  <td>{}</td>
+                  {vip(appointment.vip)}
+                  <td>{appointment.vin}</td>
+                  <td>{appointment.customer_name}</td>
+                  {/* <td>{time(appointment.date)}</td>
+                  <td>{time(appointment.time)}</td> */}
+                  <td>{appointment.technician.name}</td>
+                  <td>{appointment.reason}</td>
+                  <td><button onClick={handleCancel} className="btn btn-danger">Cancel</button></td>
+                  <td><button onClick={handleFinish} className="btn btn-success">Finish</button></td>
                 </tr>
               )
             })}
